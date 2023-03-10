39c4392fc42573d3ee8140aea17c3bc4dd00f89e
Pedram b.a <111277786+Pedminer20th@users.noreply.github.com>
2023-03-10 17:32:07 +0330

    Create debug.cpp

---
+++ debug.cpp
@@ -0,0 +1,19 @@
+// Become familiar wih C++ syntax
+// Learn to debug buggy code
+
+#include <iostream>
+using namespace std ;
+
+int main()
+{
+    // Ask for your name and where live
+    cout << "What is your name ? : " ;
+    cin >> name ;
+
+    cout << "Where do you live > : " ;
+    cin >> location ;
+
+    // Say hello
+    cout << "Hello" << name <<  " from " << location 
+    return 0 ;
+}
